511abfa2bb955f31a24379a650b496983df7c8c4
ayyclipz <ayyclipz@192.168.1.16>
2025-06-14 02:53:28 +0530

    fixes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,40 +2,40 @@
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-        branch = "main",
-      },
-      filetype = "blade",
-    }
+	"nvim-treesitter/nvim-treesitter",
+	opts = function(_, opts)
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		parser_config.blade = {
+			install_info = {
+				url = "https://github.com/EmranMR/tree-sitter-blade",
+				files = { "src/parser.c" },
+				branch = "main",
+			},
+			filetype = "blade",
+		}
 
-    if not opts.ensure_installed then
-      opts.ensure_installed = {}
-    end
-    vim.list_extend(opts.ensure_installed, {
-      "lua",
-      "vim",
-      "sql",
-      "bash",
-      "html",
-      "css",
-      "javascript",
-      "php",
-      "blade",
-      "scss",
-      "python",
-      "rust",
-      "hyprlang",
-      "diff",
-      "xml",
-      "c", -- Add this
-      "cpp",
-    })
-    return opts
-  end,
+		if not opts.ensure_installed then
+			opts.ensure_installed = {}
+		end
+		vim.list_extend(opts.ensure_installed, {
+			"lua",
+			"vim",
+			"sql",
+			"bash",
+			"html",
+			"css",
+			"javascript",
+			"php",
+			"blade",
+			"scss",
+			"python",
+			"rust",
+			"hyprlang",
+			"diff",
+			"xml",
+			"c",
+			"cpp",
+		})
+		return opts
+	end,
 }
